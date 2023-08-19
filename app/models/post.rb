2a2c6355010821a1030c99ab31635e40f47224e8
Tolulope Awoyomi <tolulopeawoyomi1@gmail.com>
2023-08-19 15:29:11 -0400

    commit tree 741fb68d7c21e80362d7f171ae2d351a7638962a

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,27 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }
+
+    validate :clibait
+
+    CLIBAIT = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top \d/i,
+        /Guess/i
+    ]
+    def clibait
+
+        if CLIBAIT.none? { |bait| bait.match title }
+            errors.add(:title, "Your title needs to be more clibait-y")
+        end
+    #     unless title.include?("Won't believe") ||
+    #         title.include?("Secret") ||
+    #         title.include?("Top [number]") ||
+    #         title.include?("Guess") 
+    #         errors.add(:title, "Clibait")
+    #     end
+    end
 end
