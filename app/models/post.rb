5bb7671ff79d7c1c167be6385c853e28cf6a4ab4
Tolulope Awoyomi <tolulopeawoyomi1@gmail.com>
2023-08-19 15:31:15 -0400

    Code lab solution

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }
+
+    validate :clibait
+
+    CLIBAIT = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top \d/i,
+        /Guess/i
+    ]
+    def clibait
+
+        if CLIBAIT.none? { |bait| bait.match title }
+            errors.add(:title, "Your title needs to be more clibait-y")
+        end
+
+    end
 end
